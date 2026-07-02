18a41593b1d9323bca73a684882adecdf8a43247
Nico Rising <nico@nicorising.com>
2026-07-01 21:22:33 -0400

    Update flake and fix Electron error

--- flake.nix
+++ flake.nix
@@ -34,7 +34,12 @@
 
     let
       commonModule = {
-        nixpkgs.config.allowUnfree = true;
+        nixpkgs.config = {
+          allowUnfree = true;
+
+          # TODO: Remove once no longer needed
+          permittedInsecurePackages = [ "electron-39.8.10" ];
+        };
 
         home-manager = {
           useGlobalPkgs = true;
