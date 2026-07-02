7be6bea924d4c64630fe95d2f0cdc48d46579203
Nico Rising <nico@nicorising.com>
2026-07-01 23:08:59 -0400

    Garbage collect generations

--- linux/configuration.nix
+++ linux/configuration.nix
@@ -5,10 +5,15 @@
     ./hardware-configuration.nix
   ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+      timeout = 1;
+    };
 
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+    kernelPackages = pkgs.linuxPackages_latest;
+  };
 
   networking = {
     hostName = "nicotop";
@@ -31,20 +36,23 @@
       ];
     };
   };
+
   services.mullvad-vpn.enable = true;
   services.mullvad-vpn.package = pkgs.mullvad-vpn;
 
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "en_US.UTF-8";
-    LC_IDENTIFICATION = "en_US.UTF-8";
-    LC_MEASUREMENT = "en_US.UTF-8";
-    LC_MONETARY = "en_US.UTF-8";
-    LC_NAME = "en_US.UTF-8";
-    LC_NUMERIC = "en_US.UTF-8";
-    LC_PAPER = "en_US.UTF-8";
-    LC_TELEPHONE = "en_US.UTF-8";
-    LC_TIME = "en_US.UTF-8";
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "en_US.UTF-8";
+      LC_IDENTIFICATION = "en_US.UTF-8";
+      LC_MEASUREMENT = "en_US.UTF-8";
+      LC_MONETARY = "en_US.UTF-8";
+      LC_NAME = "en_US.UTF-8";
+      LC_NUMERIC = "en_US.UTF-8";
+      LC_PAPER = "en_US.UTF-8";
+      LC_TELEPHONE = "en_US.UTF-8";
+      LC_TIME = "en_US.UTF-8";
+    };
   };
 
   users.users.nico = {
@@ -59,10 +67,18 @@
     ];
   };
 
-  nix.settings.experimental-features = [
-    "nix-command"
-    "flakes"
-  ];
+  nix = {
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+
+    settings.experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
 
   powerManagement.cpuFreqGovernor = "powersave";
 
@@ -120,21 +136,24 @@
   };
 
   # Shared libraries for dynamically linked binaries
-  programs.nix-ld.enable = true;
-  programs.nix-ld.libraries = with pkgs; [
-    glib
-    libGL
-    libice
-    libsm
-    libx11
-    libxcb
-    libxext
-    libxi
-    libxkbcommon
-    stdenv.cc.cc.lib
-    wayland
-    zlib
-  ];
+  programs.nix-ld = {
+    enable = true;
+
+    libraries = with pkgs; [
+      glib
+      libGL
+      libice
+      libsm
+      libx11
+      libxcb
+      libxext
+      libxi
+      libxkbcommon
+      stdenv.cc.cc.lib
+      wayland
+      zlib
+    ];
+  };
 
   programs.neovim = {
     enable = true;
