93d693585fb05033378af4300f6bf79a48d8d4f4
Nico Rising <nico@nicorising.com>
2026-07-21 17:51:05 -0400

    Add treehouse to Starship

--- programs/starship.nix
+++ programs/starship.nix
@@ -5,6 +5,13 @@
     settings = {
       aws.disabled = true;
       package.disabled = true;
+
+      custom.treehouse = {
+        when = ''[ -n "$TREEHOUSE_DIR" ]'';
+        command = ''basename "$(dirname "$TREEHOUSE_DIR")"'';
+        symbol = "🌳 ";
+        shell = [ "sh" ];
+      };
     };
   };
 }
